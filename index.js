9d4d8433004decda5fb0837778f6fb7d50b62f44
Eric <ekellogg90@gmail.com>
2024-06-10 17:17:44 -0500

    run function and shapes cleanup

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-// const Shapes = require('./lib/shapes.js');
 const Input = require('./lib/input.js');
 
-// const shapes = new Shapes();
 const input = new Input();
 
-// shapes.shapeName();
-input.shapeName();
+input.runShape();
